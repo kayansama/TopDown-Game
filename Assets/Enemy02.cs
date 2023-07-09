a7bb2495b83a3c07409d3f90c2a923d00a65ecff
kayansama <contact.kayansama@gmail.com>
2023-07-10 00:50:42 +0100

    .5 theme and explosions 03
    in the title

--- Assets/Enemy02.cs
+++ Assets/Enemy02.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Enemy02 : InteractionAbstract
 {
@@ -11,13 +12,17 @@ public class Enemy02 : InteractionAbstract
     GameObject playerGameObject;
     public float rotationSpeed = 100f;
     private int hp = 100;
-
+    public GameObject explosionParticle;
     public override void Interact()
     {
         if (hp <= 0)
         {
+            Vector3 Position = transform.position;
+            explosionParticle.gameObject.SetActive(true);
             GameManager.Instance.AddScore(1);
             GameManager.Instance.HealPlayer(10);
+            Instantiate(explosionParticle, Position, Quaternion.identity);
+            
             Destroy(gameObject); 
         }
         if (hp > 1)
