5a5cdcb376b7bccfec2c7a79a5db9a87ca67c622
kayansama <contact.kayansama@gmail.com>
2023-07-10 01:09:45 +0100

    .52

---
+++ Assets/2D Space Kit/Scripts/PSDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PSDestroy : MonoBehaviour {
+	//public GameObject ParentGameobject;
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, GetComponent<ParticleSystem>().duration);
+		//Destroy(ParentGameobject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
